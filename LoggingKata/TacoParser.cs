f56ffa2b2ed7aa9bdd001a69f204f30e69efe4e0
Aaron Carlson <acarlson2@carthage.edu>
2024-06-25 11:49:00 -0500

    exercise complete

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -18,35 +18,38 @@
             if (cells.Length < 3)
             {
                 // Log error message and return null
+                logger.LogError("Array length is less than 3");
                 return null; 
             }
 
             // TODO: Grab the latitude from your array at index 0
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            double.TryParse(cells[0], out var lat);
+
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            double.TryParse(cells[1], out var lon);
+
             // TODO: Grab the name from your array at index 2
-            
+            string name = cells[2];
 
             // TODO: Create a TacoBell class
             // that conforms to ITrackable
-            
+
             // TODO: Create an instance of the Point Struct
             // TODO: Set the values of the point correctly (Latitude and Longitude) 
+            Point point = new Point() { Latitude = lat, Longitude = lon };
 
             // TODO: Create an instance of the TacoBell class
             // TODO: Set the values of the class correctly (Name and Location)
+            TacoBell tacoBell = new TacoBell() { Name = name, Location = point };
 
             // TODO: Then, return the instance of your TacoBell class,
             // since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
